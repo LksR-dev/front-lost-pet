bfb920615c9eba178debe570f941ebca3c7d5ba9
Lucas <lucasmruiz05@gmail.com>
2022-10-10 14:18:53 -0300

    remove hook

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { SearchForm } from './SearchForm';
-import { useSearchResults } from '../hooks';
 import css from './layout.css';
 
 function Layout() {
-	const handleSearch = (search) => {};
 	return (
 		<>
 			<header className={css.root}>
